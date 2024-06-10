80fea7bb517cf89032ff2cc946fbd6f728594489
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2024-06-10 06:16:34 +0200

    Updated: <.cs> file updated.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using lib.app; //lib.app namespace using as a library.
+using libApp; //libApp namespace using as a library.
 
 //Main. Program used as a Main file executable.
 public class Program{
   // static void Main is the executable default program in a csharp program.
   public static void Main(string[] args){
     // args: the argument given through command line.
-    TestApp application = new TestApp(); // TestApp instantiation.
+    MainJClass application = new MainJClass(); // TestApp instantiation.
     // Checking if an argument has been given.
-    if(args.Length != 0){
+    var input = 0;
+    var mult = 0.0f;
+    if(args.Length == 1){
       System.Console.WriteLine("Getting value through command line... ");
-      var input = 0;
       string theString = string.Join(" ",args);
       // managing the data type given.
       try
@@ -23,11 +24,28 @@ public class Program{
       }
       application.SetValue(input); // using SetValue mehtod.
     }
+    else if (args.Length == 2)
+    {
+      try
+      {   
+          string theString = string.Join(" ",args[0]);
+          input = Int32.Parse(theString);
+          mult = float.Parse(args[1]);
+          application.SetValue(input); // using SetValue mehtod.
+      }
+      catch (System.Exception)
+      {
+        System.Console.WriteLine("[!] >> Wrong type as argument 2 {0} | {1}",mult,input);
+      }
+    }
     else{ // default value.
+
       application.SetValue(717); // using SetValue mehtod.
     }
     var myValue = application.GetValue(); // using GetValue method.
+    var output = application.Multiply(mult);
     System.Console.WriteLine("The value set: {0}",myValue);
     System.Console.WriteLine($"The property: {application.TheValue}");
+    System.Console.WriteLine("The multiplication result is: {0}",output);
   }
 }
